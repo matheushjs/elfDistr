8873211afcc08feef5f9931c1e8b09d0c05eaa78
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2019-12-06 15:14:26 -0300

    Finish comparing flexsurv and our package

--- playground/flexsurv_comparison.r
+++ playground/flexsurv_comparison.r
@@ -2,19 +2,28 @@ require(flexsurv)
 require(elfDistr)
 require(microbenchmark)
 
-flexsurv.test = function(){
-	x = runif(0, 10, n=10000);
-	return( sum(dgengamma(x, 1, 1, 1, log=T)) );
+flexsurv.test = function(n = 10000){
+	x = runif(0, 10, n=n);
+	a = rep(1, 13);
+	b = rep(1, 64);
+	c = rep(1, 37);
+	return( sum(dgengamma(x, a, b, c, log=T)) );
 }
 
-flexsurv.test.orig = function(){
-	x = runif(0, 10, n=10000);
-	return( sum(dgengamma.orig(x, 1, 1, 1, log=T)) );
+flexsurv.test.orig = function(n = 10000){
+	x = runif(0, 10, n=n);
+	a = rep(1, 13);
+	b = rep(1, 64);
+	c = rep(1, 37);
+	return( sum(dgengamma.orig(x, a, b, c, log=T)) );
 }
 
-elf.test = function(){
-	x = runif(0, 10, n=10000);
-	return( sum(dggamma(x, 1, 1, 1, log=T)) );
+elf.test = function(n = 10000){
+	x = runif(0, 10, n=n);
+	a = rep(1, 13);
+	b = rep(1, 64);
+	c = rep(1, 37);
+	return( sum(dggamma(x, a, b, c, log=T)) );
 }
 
 result = microbenchmark(
@@ -43,3 +52,20 @@ check(dggamma(x, 0.5, 1, 5), dgengamma.orig(x, 0.5, 1, 5))
 check(dggamma(x, 1, 10, 1), dgengamma.orig(x, 1, 10, 1))
 check(dggamma(x, 3, 3, 1), dgengamma.orig(x, 3, 3, 1))
 check(dggamma(x, 5, 0.3, 1), dgengamma.orig(x, 5, 0.3, 1))
+
+df = NULL
+seqN = seq(64, 10000, length=100)
+for(n in seqN){
+	cat("n = ", n, "\n");
+	result = microbenchmark(
+		flexsurv.test(n),
+		elf.test(n),
+		unit="ms",
+		times=400,
+		control=list(warmup=10)
+	);
+	
+	df = rbind(df, summary(result)$mean);
+}
+
+print(df)
